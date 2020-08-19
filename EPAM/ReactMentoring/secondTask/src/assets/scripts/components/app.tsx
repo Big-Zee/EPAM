2954832a81c8cc6cb19646c528de4c3b6fae9469
Zbigniew Koltuniak <zbigniew_koltuniak@epam.com>
2020-08-19 21:47:00 +0200

    Initial commt of second task

---
+++ EPAM/ReactMentoring/secondTask/src/assets/scripts/components/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import '../../styles/styles.scss';
+
+export interface IAppProps {}
+
+export default function IApp(props: IAppProps) {
+  return <h1>Hello React Typescript!</h1>;
+}
